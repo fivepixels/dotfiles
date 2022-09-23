b7ee0b7417da78f82a855b2e6d35213158c71bbf
cattynip <cattynip.cattynip@gmail.com>
2022-09-22 22:52:45 -0700

    [ADD] - Add Exa command

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -14,6 +14,7 @@ alias ls "ls -p -G"
 alias la "ls -A"
 alias ll "ls -l"
 alias lla "ll -A"
+alias list "exa -a  -B -b -i -g -h -l -m -s type -U --git --header --icons"
 alias g git
 
 alias tl "tmux ls"
