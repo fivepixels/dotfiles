e7ec7563b97b85a6f4e4fa08f5ccb7bbb639065c
cattynip <hi@cattynip.dev>
2024-01-22 19:28:02 -0500

    feat: reset all neovim settings

--- fish/config.fish
+++ fish/config.fish
@@ -17,8 +17,8 @@ set -g theme_hostname always
 
 # Aliases
 alias asdf "brew update && brew upgrade && flutter upgrade"
-alias l "eza -a  -l -s type --git --header --icons --no-user --no-filesize --no-time"
-alias t "find . -print | sed -e 's;[^/]*/;|____;g;s;____|; |;g'"
+alias l "eza -a -l -s type --git --header --icons --no-user --no-filesize --no-time"
+alias t "eza --tree"
 alias remove "rm -rf ~/.local/share/nvim/swap && rm ~/.local/state/nvim/lsp.log && rm -rf ~/.local/state/nvim/swap"
 alias to touch
 alias mk mkdir
@@ -59,3 +59,41 @@ set LOCAL_CONFIG (dirname (status --current-filename))/config-local.fish
 if test -f $LOCAL_CONFIG
     source $LOCAL_CONFIG
 end
+
+#!/usr/bin/fish
+
+# Kanagawa Fish shell theme
+# A template was taken and modified from Tokyonight:
+# https://github.com/folke/tokyonight.nvim/blob/main/extras/fish_tokyonight_night.fish
+set -l foreground DCD7BA normal
+set -l selection 2D4F67 brcyan
+set -l comment 727169 brblack
+set -l red C34043 red
+set -l orange FF9E64 brred
+set -l yellow C0A36E yellow
+set -l green 76946A green
+set -l purple 957FB8 magenta
+set -l cyan 7AA89F cyan
+set -l pink D27E99 brmagenta
+
+# Syntax Highlighting Colors
+set -g fish_color_normal $foreground
+set -g fish_color_command $cyan
+set -g fish_color_keyword $pink
+set -g fish_color_quote $yellow
+set -g fish_color_redirection $foreground
+set -g fish_color_end $orange
+set -g fish_color_error $red
+set -g fish_color_param $purple
+set -g fish_color_comment $comment
+set -g fish_color_selection --background=$selection
+set -g fish_color_search_match --background=$selection
+set -g fish_color_operator $green
+set -g fish_color_escape $pink
+set -g fish_color_autosuggestion $comment
+
+# Completion Pager Colors
+set -g fish_pager_color_progress $comment
+set -g fish_pager_color_prefix $cyan
+set -g fish_pager_color_completion $foreground
+set -g fish_pager_color_description $comment
--- install/development.sh
+++ install/development.sh
@@ -2,4 +2,10 @@ brew install --cask iterm2 github sourcetree pgadmin4
 
 brew install node nvm git yarn exa neovim neovide tmux planetscale/tap/pscale mysql-client cmatrix ripgrep fd postgresql@16
 
+brew tap homebrew/cask-fonts
+brew install --cask font-fira-code
+
 yarn global add eslint_d prettier sql-language-server
+
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
