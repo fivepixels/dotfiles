d38e7f5596d85fb6e0c85741474d6eeda6fdacf9
Seol So <hi@cattynip.dev>
2024-03-10 23:06:13 -0400

    feat: add plugins list

--- nvim/lua/cattynip/plugins.lua
+++ nvim/lua/cattynip/plugins.lua
@@ -1,4 +1,51 @@
 vim.opt.rtp:prepend(vim.fn.stdpath("data") .. "/pckr/pckr.nvim")
 
-require('pckr').add{
-}
+require('pckr').add({
+  -- basic
+  "nvim-lua/plenary.nvim",
+  "nvim-tree/nvim-web-devicons",
+  "MunifTanjim/nui.nvim",
+
+  -- coding
+  "neovim/nvim-lspconfig",
+  "nvimtools/none-ls.nvim",
+  "williamboman/mason.nvim",
+  "hrsh7th/nvim-cmp",
+  "mfussenegger/nvim-lint",
+  "stevearc/conform.nvim",
+
+  "L3MON4D3/LuaSnip",
+  "echasnovski/mini.pairs",
+  "echasnovski/mini.surround",
+  "echasnovski/mini.ai",
+  "echasnovski/mini.comment",
+  "JoosepAlviste/nvim-ts-context-commentstring",
+
+  "lewis6991/gitsigns.nvim",
+
+  -- editing
+  "nvim-neo-tree/neo-tree.nvim",
+  "nvim-telescope/telescope.nvim",
+  "RRethy/vim-illuminate",
+  "folke/todo-comments.nvim",
+  "windwp/nvim-ts-autotag",
+  "hedyhli/outline.nvim",
+  "SmiteshP/nvim-navic",
+  "stevearc/aerial.nvim",
+
+  -- ui
+  "craftzdog/solarized-osaka.nvim",
+  "nvim-treesitter/nvim-treesitter",
+  "nvim-treesitter/nvim-treesitter-context",
+  "akinsho/bufferline.nvim",
+  "nvim-lualine/lualine.nvim",
+  "lukas-reineke/indent-blankline.nvim",
+  "nvimdev/dashboard-nvim",
+
+  -- language specific
+  "wookayin/semshi",              -- python
+  "mrcjkb/rustaceanvim",          -- rust
+  "mfussenegger/nvim-jdtls",      -- java
+  "b0o/SchemaStore.nvim",         -- json
+  "iamcco/markdown-preview.nvim", -- markdown
+})
